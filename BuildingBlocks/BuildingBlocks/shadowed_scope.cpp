914e1d3350b01cb3eaf85dfe67279771bd23aff0
hlule <l.hunkar@gmail.com>
2020-12-13 04:45:51 -0500

    shadow explanation.

--- BuildingBlocks/BuildingBlocks/shadowed_scope.cpp
+++ BuildingBlocks/BuildingBlocks/shadowed_scope.cpp
@@ -5,6 +5,10 @@ using namespace std;
 Shadowing
 
 A name declared within the scope of an identical name shadows the entity with the broader scope.  Shadowing of this form should be avoided to improve readability and maintainability.
+
+The name of a variable declared within a member function shadows the instance variable with an identical name.  The name of the shadowed instance variable can be accessed using the this keyword (this->name).
+
+The name of a variable within a class or a function shadows the entity with an identical name and global scope.  The name of the shadowed global variable can be accessed using the scope resolution operator (::name).
 */
 int main()
 {
